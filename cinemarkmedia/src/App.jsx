d9a1da88fa815e1336caadaa326eb28502460d24
MAC <mac@Apples-MacBook-Pro.local>
2024-02-24 20:15:47 +0530

    data

--- cinemarkmedia/src/App.jsx
+++ cinemarkmedia/src/App.jsx
@@ -5,11 +5,16 @@ import './index.css'
 function App() {
   const [showCount, setShowCount] = useState(8);
   const [isActive, setIsActive] = useState(false);
+  const [menubar,setMenubar]=useState(true)
   const handleClick = () => {
     console.log("data")
-    setIsActive(!isActive);
-
+    setIsActive(true);
+    setMenubar(false)
   };
+  const closesidebar=()=>{
+    setIsActive(false)
+    setMenubar(true)
+  }
   const handleShowMore = () => {
     setShowCount(prevCount => prevCount + 8);
   };
@@ -25,6 +30,43 @@ function App() {
       });
     });
   }, []);
+
+//  Form Validation
+
+const [formData, setFormData] = useState({
+  fullName: { value: '', touched: false },
+  mobileNumber: { value: '', touched: false },
+  email: { value: '', touched: false },
+  message: { value: '', touched: false }
+});
+
+console.log(formData)
+const handleChange = (e) => {
+  const { name, value } = e.target;
+  setFormData(prevState => ({
+    ...prevState,
+    [name]: { value, touched: true }
+  }));
+};
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  // Perform form submission or validation here
+  // For simplicity, let's just log the form data
+  console.log(formData);
+};
+
+const validateMobileNumber = (mobileNumber) => {
+  // Regular expression for validating Indian mobile numbers
+  const mobileRegex = /^[6-9]\d{9}$/;
+  return mobileRegex.test(mobileNumber);
+};
+
+const validateEmail = (email) => {
+  // Regular expression for validating email addresses
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
 
   const workDoneImg = [
     {
@@ -86,26 +128,26 @@ function App() {
   ]
   return (
     <>
-      <div className=' flex px-8 py-6	 bg-gray-600  justify-between  w-[100%] position: sticky top-0 z-10' id='home'>
+      <div className={ `flex px-8 py-6 transition-all duration-500	transition-all duration-500 bg-gray-600  justify-between  w-[100%] position: sticky top-0 z-10`} id='home'>
        
-        <ul className={` hidden md:hidden lg:flex  w-[100%] justify-between  ${isActive ? 'absolute top-0  block left-0 bg-black ' : ''}`}>
-      <div>  <a href="" className='text-white-600 text-white'>CINE MARK MEDIA</a></div>
-      <div className='flex gap-x-[50px]'>
-          <li className='mx-2 text-white hover:text-blue-500  hover:underline transition duration-300 ease-in-out'><a href="/">Home</a></li>
-          <li className='mx-2 text-white  hover:text-blue-500  hover:underline transition duration-300 ease-in-out '><a href="#about">About Us</a></li>
-          <li className='mx-2 text-white  hover:text-blue-500  hover:underline transition duration-300 ease-in-out'><a href="#services">Services</a></li>
-          <li className='mx-2 text-white  hover:text-blue-500  hover:underline transition duration-300 ease-in-out'><a href="#work">Our Work</a></li>
-          <li className='mx-2 text-white  hover:text-blue-500  hover:underline transition duration-300 ease-in-out'><a href="#contact">Contact</a></li>
+        <ul className={`${isActive?"transition-all duration-500 flex-col md:flex-col lg:flex  h-screen md:screen ":"hidden md:hidden lg:flex"}  w-[100%] justify-between`}>
+      <a href="" className='text-white-600 text-white'>CINE MARK MEDIA</a>
+      <div className={`${isActive?"transition-all duration-500 flex-col md:flex-col lg:flex  items-center mt-[50px]":""} flex gap-x-[50px]`}>
+          <li className='mx-2 text-white hover:text-blue-500  hover:underline transition duration-300 ease-in-out mb-5 lg:mb-0 font-nunito'><a href="/">Home</a></li>
+          <li className='mx-2 text-white  hover:text-blue-500  hover:underline transition duration-300 ease-in-out mb-5 lg:mb-0 font-nunito'><a href="#about">About Us</a></li>
+          <li className='mx-2 text-white  hover:text-blue-500  hover:underline transition duration-300 ease-in-out mb-5 lg:mb-0 font-nunito'><a href="#services">Services</a></li>
+          <li className='mx-2 text-white  hover:text-blue-500  hover:underline transition duration-300 ease-in-out mb-5 lg:mb-0 font-nunito'><a href="#work">Our Work</a></li>
+          <li className='mx-2 text-white  hover:text-blue-500  hover:underline transition duration-300 ease-in-out font-nunito'><a href="#contact">Contact</a></li>
           </div>
        
         </ul>
-        {  isActive && <img  className={` ${isActive ? 'block' : 'bg-gray-300 text-black'} h-32 w-32`} src='/img/cross.png'></img>}
-           <img  className={`block lg:hidden `}  onClick={handleClick} src='/img/hamburger.svg'></img>
+        { isActive && <img  className={`block md:block lg:hidden 'bg-gray-300 text-black'} h-10 w-10`} onClick={closesidebar} src='/img/cross.png'></img>}
+       {menubar && <img  className={"block md:block lg:hidden absolute top-3 right-5"}  onClick={handleClick} src='/img/hamburger.svg' height={30} width={30}></img>}
       </div>
       <div className='bg-hero-pattern h-screen bg-no-repeat bg-center object-cover  flex  justify-center items-center opacity-70 top-0 left-0 '>
         <div className='text-center'>
-          <h1 className='text-[100px]  text-white'>Suraj Kumar Review</h1>
-          <h2 className='text-[75px] text-white' > Hardik && Shubham && Sidhu</h2>
+          <h1 className='text-[100px]  text-white'>suraj padwa sala</h1>
+          <h2 className='text-[75px] text-white' > Hardik mota bhosdi ka</h2>
         </div>
       </div>
       {/* About us */}
@@ -113,7 +155,7 @@ function App() {
         <div className='flex justify-between items-center flex-col md:flex-col lg:flex-row'>
           <div className='text-center'>
             <h2 className='text-[28px] leading-relaxed font-sans font-bold text-gray-600 text-white'>About Us</h2>
-            <p className='max-w-[700px] text-[18px]  md:text-[24px] font-sans text-white mt-5 '>Cinemark Media is a dynamic digital marketing agency that combines
+            <p className='max-w-[700px] text-[18px] font-nunito  md:text-[24px] font-sans text-white mt-5 '>Cinemark Media is a dynamic digital marketing agency that combines
               creativity and strategic thinking to deliver innovative and effective solutions
               for your projects. With a focus on cutting-edge digital trends, Cinemark Media is dedicated
               to elevating your online presence and maximizing your impact in the digital landscape.</p>
@@ -124,33 +166,33 @@ function App() {
 
         {/* our services */}
         <div className='text-center mt-[100px]' >
-          <h2 className='text-[32px] leading-relaxed font-sans font-bold  text-white' id='services'>Our Services</h2>
-          <p className='font-sans text-white  text-[18px]  md:text-[24px] mt-5 '>DG Avatar specialises in all types of branding solutions and primarily serves the Film and Entertainment sector. We provide the following services:</p>
+          <h2 className='text-[32px] leading-relaxed font-sans font-bold font-sans  text-white' id='services'>Our Services</h2>
+          <p className='font-sans text-white  text-[18px]  md:text-[24px] mt-5  font-nunito'>DG Avatar specialises in all types of branding solutions and primarily serves the Film and Entertainment sector. We provide the following services:</p>
           <div className='flex flex-wrap gap-[32px] mt-[40px] justify-center'>
             <div className='flex flex-col items-center  bg-[#e6f9ff] rounded-[30px] p-[15px] shadow-md transition duration-300 ease-in-out transform hover:scale-105'><img src="https://dgavatar.in/wp-content/uploads/2022/10/Social-Media-300x300.png" alt="logo" height={300} width={300}></img>
-              <p className='font-sans text-gray-700 '>Social Media Management</p>
+              <p className='font-sans text-gray-700 font-nunito  '>Social Media Management</p>
             </div>
             <div className='flex flex-col items-center  bg-[#e6f9ff] rounded-[30px] p-[15px] shadow-md transition duration-300 ease-in-out transform hover:scale-105'><img src="https://dgavatar.in/wp-content/uploads/2022/10/Social-Media-300x300.png" alt="logo" height={300} width={300}></img>
-              <p className='font-sans text-gray-700 '>Social Media Management</p>
+              <p className='font-sans text-gray-700 font-nunito '>Social Media Management</p>
             </div>
             <div className='flex flex-col items-center  bg-[#e6f9ff] rounded-[30px] p-[15px] shadow-md transition duration-300 ease-in-out transform hover:scale-105'><img src="https://dgavatar.in/wp-content/uploads/2022/10/Social-Media-300x300.png" alt="logo" height={300} width={300}></img>
-              <p className='font-sans text-gray-700 '>Social Media Management</p>
+              <p className='font-sans text-gray-700 font-nunito '>Social Media Management</p>
             </div>
             <div className='flex flex-col items-center  bg-[#e6f9ff] rounded-[30px] p-[15px] shadow-md transition duration-300 ease-in-out transform hover:scale-105'><img src="https://dgavatar.in/wp-content/uploads/2022/10/Social-Media-300x300.png" alt="logo" height={300} width={300}></img>
-              <p className='font-sans text-gray-700 '>Social Media Management</p>
+              <p className='font-sans text-gray-700 font-nunito '>Social Media Management</p>
             </div>
             <div className='flex flex-col items-center  bg-[#e6f9ff] rounded-[30px] p-[15px] shadow-md transition duration-300 ease-in-out transform hover:scale-105'><img src="https://dgavatar.in/wp-content/uploads/2022/10/Social-Media-300x300.png" alt="logo" height={300} width={300}></img>
-              <p className='font-sans text-gray-700 '>Social Media Management</p>
+              <p className='font-sans text-gray-700 font-nunito '>Social Media Management</p>
             </div>
             <div className='flex flex-col items-center  bg-[#e6f9ff] rounded-[30px] p-[15px]shadow-md transition duration-300 ease-in-out transform hover:scale-105'><img src="https://dgavatar.in/wp-content/uploads/2022/10/Social-Media-300x300.png" alt="logo" height={300} width={300}></img>
-              <p className='font-sans text-gray-700'>Social Media Management</p>
+              <p className='font-sans text-gray-700 font-nunito '>Social Media Management</p>
             </div>
           </div>
         </div>
         {/* work done by us */}
 
         <div className='text-center mt-[100px]' id='work' >
-          <h3 className=' text-center mt-[100px] text-[32px] leading-relaxed font-sans font-bold text-white' id='contact'
+          <h3 className=' text-center mt-[100px] text-[32px] leading-relaxed font-sans font-bold text-white font-sans' id='contact'
           >Our Work</h3>
           <div className='flex flex-wrap gap-[32px] mt-[40px] justify-center'>
 
@@ -174,29 +216,71 @@ function App() {
 
         
         {/* form data */}
-        <h3 className=' text-center mt-[100px] text-[32px] leading-relaxed font-sans font-bold text-white text-[18px]  md:text-[24px]'>Contact Us</h3>
+        <h3 className=' text-center mt-[100px] text-[32px] leading-relaxed font-sans font-bold text-white text-[18px]  md:text-[24px] font-sans'>Contact Us</h3>
         <div className="">
-          <p className='font-sans text-white text-[32px] font-bold mb-[30px] text-[18px]  text-center md:text-start md:text-[24px]'>Start a project with us today!</p>
-          <form className="bg-black shadow-md rounded px-8 pt-6 pb-8 mb-4">
-            <div className="mb-10">
-              <input className="shadow appearance-none text-white  w-full py-2  text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-black border-b-[2px]" id="full-name" type="text" placeholder='Enter Your Name' />
-            </div>
-            <div className="mb-10" id='contact'>
-              <input className="  text-white shadow appearance-none w-full py-2  text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-black border-b-[2px]" id="contact" type="text" placeholder='Enter Mobile Number' />
-            </div>
-            <div className="mb-10">
-              <input className=" text-white shadow appearance-none w-full py-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-black border-b-[2px]" id="contact" type="text" placeholder='Enter Email' />
-            </div>
-            <div className="mb-6">
-
-              <textarea className=" text-white shadow appearance-none border-b-[3px] w-full  text-gray-700 leading-tight focus:outline-none  bg-black focus:shadow-outline" id="message" placeholder='Write message here....'></textarea>
-            </div>
-            <div className="flex items-center justify-between "  >
-              <button className="bg-white hover:bg-gray-700 hover:text-white text-black font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline" type="button">
-                Submit
-              </button>
-            </div>
-          </form>
+          <p className='font-sans text-white text-[32px] font-bold mb-[30px] text-[18px]  text-center md:text-start md:text-[24px] font-sans'>Start a project with us today!</p>
+          <form className="bg-black shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+      <div className="mb-10">
+        <input
+          className="shadow appearance-none text-white w-full font-nunito py-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-black border-b-[1px]"
+          id="full-name"
+          type="text"
+          name="fullName"
+          value={formData.fullName.value}
+          onChange={handleChange}
+          placeholder='Enter Your Name'
+          required
+        />
+      </div>
+      <div className="mb-10" id='contact'>
+        <input
+          className="text-white shadow appearance-none w-full font-nunito  py-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-black border-b-[1px]"
+          id="contact"
+          type="text"
+          name="mobileNumber"
+          value={formData.mobileNumber.value}
+          onChange={handleChange}
+          placeholder='Enter Mobile Number'
+          pattern="[0-9]*"
+          minLength="10"
+          maxLength="10"
+          required
+        />
+        {formData.mobileNumber.touched && !validateMobileNumber(formData.mobileNumber.value) && <p className="text-red-500 font-nunito">Please enter a valid mobile number</p>}
+      </div>
+      <div className="mb-10">
+        <input
+          className="text-white shadow appearance-none w-full font-nunito py-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-black border-b-[1px]"
+          id="contact"
+          type="email"
+          name="email"
+          value={formData.email.value}
+          onChange={handleChange}
+          placeholder='Enter Email'
+          required
+        />
+          {formData.email.touched && !validateEmail(formData.email.value) && <p className="text-red-500 font-nunito">Please enter a valid email address</p>}
+      </div>
+      <div className="mb-6">
+        <textarea
+          className="text-white shadow appearance-none border-b-[1px]  font-nunito w-full text-gray-700 leading-tight focus:outline-none bg-black focus:shadow-outline"
+          id="message"
+          name="message"
+          value={formData.message.value}
+          onChange={handleChange}
+          placeholder='Write message here....'
+          required
+        />
+      </div>
+      <div className="flex items-center justify-between">
+        <button
+          className={`${formData?"bg-white":"bg-gray-400"} hover:bg-gray-700 font-nunito hover:text-white text-black font-bold py-2 px-6 rounded focus:outline-none focus:shadow-outline`}
+          type="submit"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
         </div>
         {/* Footer */}
         <div className='bg-white h-1'></div>
@@ -216,11 +300,11 @@ function App() {
           </div>
           <div>
             <ul className='flex  md:mt-10 mt-5'>
-              <li className='mx-2 text-red'><a href=""><img src="https://cdn-icons-png.flaticon.com/128/2111/2111463.png" alt="" height={50} width={50} /></a></li>
-              <li className='mx-2 text-red'><a href=""><img src="https://cdn-icons-png.flaticon.com/128/4494/4494485.png" alt="" height={50} width={50} /></a></li>
-              <li className='mx-2 text-red'><a href=""><img src="https://cdn-icons-png.flaticon.com/128/5968/5968764.png" alt="" height={50} width={50} /></a></li>
-              <li className='mx-2 text-red'><a href=""><img src="https://cdn-icons-png.flaticon.com/128/733/733585.png" alt="" height={50} width={50} /></a></li>
-              <li className='mx-2 text-red'><a href=""><img src="https://cdn-icons-png.flaticon.com/128/5969/5969020.png" alt="" height={50} width={50} /></a></li>
+              <li className='mx-1 text-red'><a href=""><img src="img/insta.png" alt="" height={70} width={70} /></a></li>
+              <li className='mx-1 text-red'><a href=""><img src="img/fb.png" alt="" height={70} width={70} /></a></li>
+              <li className='mx-1 text-red'><a href=""><img src="img/what.png" alt="" height={70} width={70} /></a></li>
+              <li className='mx-1 text-red'><a href=""><img src="img/insta.png" alt="" height={70} width={70} /></a></li>
+              <li className='mx-1 text-red'><a href=""><img src="img/fb.png" alt="" height={70} width={70} /></a></li>
             </ul>
             <p className='font-sans text-gray-600 mt-[10px] max-w-[300px] text-center mt-7 '>Juhu Tara Rd, Uditi Tarang Housing Colony, Juhu Tara, Juhu, Mumbai, Maharashtra 400049, India</p>
           </div>
